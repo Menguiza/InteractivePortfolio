5c063b0f8e4cffb1723ad251bafb0ec5b3b5ab6a
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2023-03-30 00:39:06 -0500

    Movement finished

--- Project/Assets/BrokenVector/LowPolyFencePack/Scripts/DoorToggle.cs
+++ Project/Assets/BrokenVector/LowPolyFencePack/Scripts/DoorToggle.cs
@@ -3,29 +3,18 @@ using UnityEngine;
 
 namespace BrokenVector.LowPolyFencePack
 {
-    /// <summary>
-    /// This class toggles the door animation.
-    /// The gameobject of this script has to have the DoorController script which needs an Animator component
-    /// and some kind of Collider which detects your mouse click applied.
-    /// </summary>
-    [RequireComponent(typeof(DoorController))]
-	public class DoorToggle : MonoBehaviour
+    public class DoorToggle : MonoBehaviour
     {
-        [SerializeField] private GameObject collider; 
+        [SerializeField] private GameObject col; 
 
-        private DoorController doorController;
-
-        void Awake()
-        {
-            doorController = GetComponent<DoorController>();
-        }
+        [SerializeField] private DoorController doorController;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 doorController.ToggleDoor();
-                collider.SetActive(false);
+                col.SetActive(false);
             }
         }
         
@@ -34,7 +23,7 @@ namespace BrokenVector.LowPolyFencePack
             if (other.CompareTag("Player"))
             {
                 doorController.ToggleDoor();
-                collider.SetActive(true);
+                col.SetActive(true);
             }
         }
     }
