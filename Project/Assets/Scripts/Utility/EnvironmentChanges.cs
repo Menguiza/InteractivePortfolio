cc6577acf9dadb93d7d50a3dce897666ada8b72b
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2023-04-13 21:44:35 -0500

    Documentation

--- Project/Assets/Scripts/Utility/EnvironmentChanges.cs
+++ Project/Assets/Scripts/Utility/EnvironmentChanges.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
 
+/// <summary>
+/// This class manages environment changes.
+/// It apply specific parameters to the render settings.
+/// </summary>
 public class EnvironmentChanges : MonoBehaviour
 {
+    //Assignable parameters
     [SerializeField] private Material mat;
     [SerializeField] private Color ambientColor, lightColor;
     
-    // Start is called before the first frame update
     void Start()
+    {
+        //Method call to trigger main behavior of this class
+        ApplySettings();
+    }
+
+    /// <summary>
+    /// Use unity built-in reference to render settings and sets specific parameters
+    /// to fog, skybox, ambientSkyColor and ambientLight.
+    /// </summary>
+    void ApplySettings()
     {
         RenderSettings.fog = false;
         RenderSettings.skybox = mat;
--- Project/Assets/Scripts/Utility/PlayerFollower.cs
+++ Project/Assets/Scripts/Utility/PlayerFollower.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
+/// <summary>
+/// This class manages GameObject's position to follow an specific transform.
+/// In this case the component transform to follow is the one attached our player.
+/// </summary>
 public class PlayerFollower : MonoBehaviour
 {
+    //Assignable parameters
     [SerializeField] private Transform player;
-
-    // Update is called once per frame
+    
     void Update()
     {
+        //Method called to update constantly GameObject's position
         Follow();
     }
 
-    void Follow()
+    /// <summary>
+    /// Takes reference transform position and create a new Vector3 with each axis value.
+    /// Then it applies the result Vector3 to this object transform.position.
+    /// </summary>
+    private void Follow()
     {
         transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
     }
